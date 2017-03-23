415cca7e27cb9ad36cceef33fd644b74c03f3927
Luiz Felipe Silva <gituser@luizfelipe.eng.br>
2017-03-22 22:29:24 -0300

    working version 3

--- caching.c
+++ caching.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdint.h>
 #define BLOCKSZ 64
+#define MALLOCERROR -1
 
 struct linkedlist {
 	uint16_t blockid;
@@ -16,8 +17,9 @@ uint32_t add_block (struct linkedlist **, uint32_t, uint32_t, const uint8_t*);
 int main (int argc, char ** argv)
 {
 	const uint8_t *data = "012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789";
-	const uint8_t *data2 = "aaaaa";
 	const uint32_t datalen = strlen(data);
+
+	const uint8_t *data2 = "aaaaa111xxxAaAa";
 	const uint32_t datalen2 = strlen(data2);
 
 	mysave (&global, 100, datalen, data);
@@ -26,10 +28,16 @@ int main (int argc, char ** argv)
 
 	mysave (&global, 500, datalen, data);
 
-	mysave (&global, 100, datalen2, data2);
+	mysave (&global, 101, 2, "XX");
 
 	mysave (&global, 480, datalen2, data2);
 	mysave (&global, 525, datalen2, data2);
+
+	mysave (&global, 1000, datalen2, data2);
+	mysave (&global, 105, 2, "XX");
+
+	mysave (&global, 0, datalen2, data2);
+	mysave (&global, 5, datalen2, data2);
 
 	while (global) {
 		printf ("blockid: %d, data: %s\n", global->blockid, global->data);
@@ -43,7 +51,6 @@ uint32_t mysave (struct linkedlist **root, uint32_t offset, uint32_t len, const 
 	uint16_t i;
 	float blocks;
 	uint32_t r = 0;
-	struct linkedlist *leaf;
 	uint8_t updateroot = 0x1;
 
 	blocks = ( offset % BLOCKSZ + len ) / (float) BLOCKSZ;
@@ -56,46 +63,34 @@ uint32_t mysave (struct linkedlist **root, uint32_t offset, uint32_t len, const 
 
 	for (i = 0; i < (uint16_t) blocks; i++) {
 		uint32_t localr;
-		uint32_t roffset, rlen;
-		uint8_t * rdata = (uint8_t*) data;;
+		struct linkedlist *leaf;
+		uint32_t relaoffset, relalen;
+		uint8_t * reladata = (uint8_t*) data;
 
-		rlen = len - r;
+		relalen = len - r;
 		if (i == 0) {
 			int round = BLOCKSZ - offset%BLOCKSZ;
-			if (rlen > round) rlen = round;
+			if (relalen > round) relalen = round;
 		}
 
-		rdata = rdata + r;
-		roffset = offset + r;
+		reladata = reladata + r;
+		relaoffset = offset + r;
 
 		for (leaf = *root; leaf && leaf->next; leaf = leaf->next) {
-			if ( ( leaf->next->blockid * BLOCKSZ ) >= roffset )
+			if ( ( leaf->next->blockid * BLOCKSZ ) >= relaoffset )
 				break;
 			updateroot = 0x0;
 		}
 
+		localr = add_block(&leaf, relaoffset, (relalen > BLOCKSZ) ? BLOCKSZ : relalen, reladata);
+		if (localr == MALLOCERROR)
+			return MALLOCERROR;
 
-		localr = add_block(&leaf, roffset, (rlen > BLOCKSZ) ? BLOCKSZ : rlen, rdata);
 		r = r + localr;
 
 		if (updateroot & 0x1)
 			*root = leaf;
 	}
-
-/*
-	for ( leaf = *root; leaf->next; leaf = leaf->next ) {
-		struct linkedlist *clean;
-
-		if ( leaf->blockid != leaf->next->blockid )
-			continue;
-
-		clean = leaf->next;
-
-		memcpy ( leaf, leaf->next, sizeof (struct linkedlist) );
-
-		free ( clean );
-	}
-*/
 
 	return r;
 }
@@ -104,11 +99,12 @@ uint32_t add_block (struct linkedlist **leaf, uint32_t offset, uint32_t len, con
 	struct linkedlist *block;
 	uint16_t blockid = offset/BLOCKSZ;
 
-	printf ("%d %d\n", offset, len);
-
 	block = *leaf;
 	if (!block || ( block->blockid != blockid ) ) {
 		block = malloc ( sizeof( struct linkedlist ) );
+		if (!block)
+			return MALLOCERROR;
+
 		memset (block->data, ' ', BLOCKSZ);
 		block->blockid = blockid;
 	}
@@ -117,8 +113,13 @@ uint32_t add_block (struct linkedlist **leaf, uint32_t offset, uint32_t len, con
 		block->next = NULL;
 		*leaf = block;
 	} else if (block != *leaf) {
-		block->next = (*leaf)->next;
-		(*leaf)->next = block;
+		if (block->blockid > (*leaf)->blockid) {
+			block->next = (*leaf)->next;
+			(*leaf)->next = block;
+		} else {
+			block->next = (*leaf);
+			(*leaf) = block;
+		}
 	}
 
 	memcpy (block->data + offset%BLOCKSZ, data, len);
